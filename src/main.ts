9ef5a195cfbe919dcd6b4bf7cbe2dac848546e58
vekim00100 <vekim@ucsc.edu>
2024-11-13 11:37:00 -0800

    Step 1: Initial non-interactive UI layout (Adding app title & canvas)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import "./style.css";
 
-const APP_NAME = "Small Change";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
+const APP_NAME = "Small Change";
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+const header = document.createElement("h1");
+header.innerHTML = APP_NAME;
+app.append(header);
+
+// Creating canvas
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+app.append(canvas);
